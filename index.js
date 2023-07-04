988063b9f64ef46472b6c071f995396959f3b740
1kevinjoseph <138594810+1kevinjoseph@users.noreply.github.com>
2023-07-04 20:17:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+/*/let person={
+    name:'dan',
+    age:5
+}
+console.log(person)    
+
+const pi = 3.142/*/
+
---
+++ loops.js
@@ -0,0 +1,18 @@
+/*/let x=4,y=4
+function add ()
+{
+return x+y
+}
+console.log(x+y)/*/
+/*/for (let i = 0; i < 5; i++) {
+console.log("the number is "+ i  )
+    
+}/*/
+
+let sum =0 
+for(let i=0; i<=100; i+=2){
+    sum+=i
+    console.log(sum)
+}
+
+
