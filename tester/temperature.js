c3b4175659d86a55204f886f6173c9b78d63225b
1kevinjoseph <kevinokech3@gmail.com>
2023-07-09 22:06:03 +0300

    message

---
+++ tester/temperature.js
@@ -0,0 +1,7 @@
+let temp=20;
+if (temp>=15 && temp<=50) {
+    console.log("its hot")
+    
+}
+else
+console.log("its cold")
---
+++ tester/tester.js
@@ -0,0 +1,18 @@
+// if else condition
+let a = 10; let b = 15;
+if(a>b)
+console.log("a>b")
+else
+console.log("b>a")
+// for loop
+for(i=0;i<=10;i++){
+    console.log("the number is",i)
+}
+
+// while loop
+// let username = "";
+// while ((username == "")) {
+//   username=window.prompt("enter your name")
+// }
+// console.log("hello",username)
+
