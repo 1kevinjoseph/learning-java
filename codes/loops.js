b7f9ac1765d3ee6b60c813f97d02c29e2b19aab7
joe052 <joekaranjasenior52@gmail.com>
2023-07-10 15:30:44 +0300

    message

--- codes/loops.js
+++ codes/loops.js
@@ -1,18 +1,31 @@
-/*/let x=4,y=4
-function add ()
-{
-return x+y
-}
-console.log(x+y)/*/
-/*/for (let i = 0; i < 5; i++) {
-console.log("the number is "+ i  )
-    
-}/*/
-
-let sum =0 
-for(let i=0; i<=100; i+=2){
-    sum+=i
-    console.log(sum)
+/**Add even numbers between 1 and 100 */
+function evenAdd() {
+  let sum = 0;
+  for (let i = 0; i <= 100; i += 2) {
+    sum += i;
+  }
+  console.log(sum);
 }
 
+function evenAdd2(number) {
+  let sum = 0;
+  for (let i = 0; i <= number; i += 2) {
+    sum += i;
+  }
+  console.log(sum);
+}
 
+/**Add numbers in an array */
+let arr = [2, 6, 4, 33, 7, 1, 99, 4, 6, 3, 6, 4];
+
+function arrSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+evenAdd2(500);
+// let arrayTotal = arrSum(arr);
+// console.log(arrayTotal);
