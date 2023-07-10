bc6bb268e0cb902b9c05f4893c1e187c8a2cefc3
1kevinjoseph <kevinokech3@gmail.com>
2023-07-10 22:51:56 +0300

    message

--- codes/greatest.js
+++ codes/greatest.js
@@ -1,4 +1,13 @@
 /**Method to find the greatest number in an array */
+let arr = [4, 56, 3, 44, 35, 74];
 function checkGreatest(arr) {
   //write your code here
+  greatest = 0;
+  for (i = 0; i < arr.length; i++) {
+    greatest = arr[0] > arr[i];
+  }
+  return greatest;
 }
+checkGreatest(arr);
+let large = checkGreatest(arr);
+console.log(greatest);
