7657e9d255cdb64750031eed74024b53ded567db
joe052 <joekaranjasenior52@gmail.com>
2023-07-10 15:31:55 +0300

    message

--- codes/loops.js
+++ codes/loops.js
@@ -1,13 +1,5 @@
-/**Add even numbers between 1 and 100 */
-function evenAdd() {
-  let sum = 0;
-  for (let i = 0; i <= 100; i += 2) {
-    sum += i;
-  }
-  console.log(sum);
-}
-
-function evenAdd2(number) {
+/**Add even numbers between 1 and given number */
+function evenAdd(number) {
   let sum = 0;
   for (let i = 0; i <= number; i += 2) {
     sum += i;
@@ -26,6 +18,6 @@ function arrSum(arr) {
   return sum;
 }
 
-evenAdd2(500);
+// evenAdd(500);
 // let arrayTotal = arrSum(arr);
 // console.log(arrayTotal);
