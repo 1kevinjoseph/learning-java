b331638dbb5781890a26ee41bcf5d74f359d84dc
Kevin Okech <138594810+1kevinjoseph@users.noreply.github.com>
2023-07-11 14:49:30 +0300

    Update greatest.js

--- codes/greatest.js
+++ codes/greatest.js
@@ -2,12 +2,12 @@
 let arr = [4, 56, 3, 44, 35, 74];
 function checkGreatest(arr) {
   //write your code here
-  greatest = 0;
+  
   for (i = 0; i < arr.length; i++) {
     greatest = arr[0] > arr[i];
   }
   return greatest;
 }
 checkGreatest(arr);
-let large = checkGreatest(arr);
+
 console.log(greatest);
