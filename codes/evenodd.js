864704e6e3c45404bda91d70cd94ad4eb68766c6
1kevinjoseph <kevinokech3@gmail.com>
2023-07-11 23:42:16 +0300

    message

--- codes/evenodd.js
+++ codes/evenodd.js
@@ -1,5 +1,22 @@
 /**Given an array of numbers write a function that will split
  *  the array into two arrays even and odd respectively*/
+const arr = [2, 4, 5, 7, 9, 11];
 function splitArr(arr) {
   //write your code here
+  // declaring split arrays
+  const even = [];
+  const odd = [];
+  number = arr[0];
+  // looping statement
+  for (i = 0; i < arr.length; i++)
+    //condition to check if number is even or odd
+    if (number % 2 === 0) {
+      number = even.push;
+    } else {
+      number = odd.push;
+    }
+  return arr;
 }
+splitArr(arr);
+const evenOdd = splitArr(arr);
+console.log(evenOdd);
--- codes/greatest.js
+++ codes/greatest.js
@@ -1,19 +1,19 @@
-/**Method to find the greatest number in an array */
-let arr = [4, 56, 3, 99, 35, 74];
+// /**Method to find the greatest number in an array */
+// let arr = [4, 56, 3, 99, 35, 74];
 
-function checkGreatest(arr) {
-  //write your code here
-  let greatest = arr[0];
+// function checkGreatest(arr) {
+//   //write your code here
+//   let greatest = arr[0];
 
-  for (i = 0; i < arr.length; i++) {
-    //if the next element is greater make it the greatest
-    if (arr[i + 1] > greatest) {
-      greatest = arr[i + 1];
-    }
-  }
-  return greatest;
-}
+//   for (i = 0; i < arr.length; i++) {
+//     //if the next element is greater make it the greatest
+//     if (arr[i + 1] > greatest) {
+//       greatest = arr[i + 1];
+//     }
+//   }
+//   return greatest;
+// }
+// checkGreatest(arr)
+// const greatest = checkGreatest(arr);
 
-const greatest = checkGreatest(arr);
-
-console.log(greatest);
+// console.log(greatest);
