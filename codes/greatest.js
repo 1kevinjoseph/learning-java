daa28b3b362dbd50994af8d1027dabc304f444e0
joe052 <joekaranjasenior52@gmail.com>
2023-07-11 15:51:12 +0300

    message

--- codes/greatest.js
+++ codes/greatest.js
@@ -2,9 +2,9 @@
 let arr = [4, 56, 3, 99, 35, 74];
 
 function checkGreatest(arr) {
+  //write your code here
   let greatest = arr[0];
 
-  //write your code here
   for (i = 0; i < arr.length; i++) {
     //if the next element is greater make it the greatest
     if (arr[i + 1] > greatest) {
