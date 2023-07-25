713c6a2dc9ca20501f2d06ba372060a8a63cdd23
joe052 <joekaranjasenior52@gmail.com>
2023-07-25 19:31:38 +0300

    message

--- codes/evenodd.js
+++ codes/evenodd.js
@@ -1,30 +1,28 @@
 /**Given an array of numbers write a function that will split
  *  the array into two arrays even and odd respectively*/
-let arr = [2, 5, 4, 7, 9, 10];
-splitArr(arr);
-evenOdd = splitArr(arr);
-console.log(evenOdd);
 function splitArr(arr) {
   //write your code here
-  // declaring split arrays
-  let even = [];
-  let odd = [];
-  number = arr[0];
-  for (i = 0; i < arr.length; i++) {
-    // looping statement
+  console.log(arr);
 
-    if (arr[i + 1] % 2 === 0) {
-      //condition to check if number is even or odd
+  //create the two arrays needed
+  let evenArr = [];
+  let oddArr = [];
+  let combArr = [];
 
-      number = even.push;
-      number = arr[i + 1];
-    } else {
-      number = odd.push;
-      number = arr[i + 1];
-    }
+  //loop into given array and split
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    //check if number is even
+    isNumEven(num) ? evenArr.push(num) : oddArr.push(num);
   }
 
-  return arr;
+  combArr.push(evenArr);
+  combArr.push(oddArr);
+  return combArr;
+}
+
+function isNumEven(num) {
+  return num % 2 == 0;
 }
 
 let arr = [1, 4, 5, 8, 34, 67, 1, 5, 9, 12, 6, 8, 90];
