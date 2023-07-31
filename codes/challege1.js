84ed34cd83912d51528f23fae900b8c91867fd00
1kevinjoseph <kevinokech3@gmail.com>
2023-07-31 20:16:19 +0300

    message

---
+++ codes/challege1.js
@@ -0,0 +1,8 @@
+// Return the Sum of Two Numbers
+// Create a function that takes two numbers as arguments and returns their sum.
+function addition(x, y) {
+  return x + y;
+}
+
+let result = addition(2, 3);
+console.log(result);
