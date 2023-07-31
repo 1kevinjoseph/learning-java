8ef30f5a73664297ec75910626242e9c1eebe543
1kevinjoseph <kevinokech3@gmail.com>
2023-07-31 20:28:57 +0300

    message

---
+++ codes/challenge2.js
@@ -0,0 +1,8 @@
+// Convert Minutes into Seconds
+// Write a function that takes an integer minutes and converts it to seconds.
+
+function convert(minutes) {
+  return minutes * 60;
+}
+let val = convert(5);
+console.log(val);
---
+++ codes/challenge3.js
@@ -0,0 +1,8 @@
+// Area of a Triangle
+// Write a function that takes the base and height of a triangle and return its area.
+
+function area(length, width, height) {
+  return length * width * height;
+}
+let ans = area(2, 5, 10);
+console.log("area is" + ans);
