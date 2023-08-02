d449c4aebf042a588c9ab3b9303a156085e55596
1kevinjoseph <kevinokech3@gmail.com>
2023-08-02 19:45:06 +0300

    message

--- codes/challenge3.js
+++ codes/challenge3.js
@@ -1,8 +1,8 @@
 // Area of a Triangle
 // Write a function that takes the base and height of a triangle and return its area.
 
-function area(length, width, height) {
-  return length * width * height;
+function area(base, height) {
+  return base * height;
 }
-let ans = area(2, 5, 10);
+let ans = area(2, 10);
 console.log("area is" + ans);
