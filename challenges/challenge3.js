3ff9160d6da949b1ef9bd7dd2d947ac65760d1fd
1kevinjoseph <kevinokech3@gmail.com>
2023-08-02 20:20:22 +0300

    message

--- challenges/challenge3.js
+++ challenges/challenge3.js
@@ -2,7 +2,7 @@
 // Write a function that takes the base and height of a triangle and return its area.
 
 function area(base, height) {
-  return base * height;
+  return base * height*0.5;
 }
-let ans = area(2, 10);
+let ans = area(8, 10);
 console.log("area is" + ans);
