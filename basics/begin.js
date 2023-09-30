b587639451fde5abbd140f996e7b204c771d074a
1kevinjoseph <kevinokech3@gmail.com>
2023-09-30 13:41:39 +0300

    message

---
+++ basics/begin.js
@@ -0,0 +1,14 @@
+//datatypes, variables and basic syntax and more
+//numbers
+let a = 6;
+let b = 12;
+var h = 2;
+const fruit = 99;
+// strings
+let name = "james";
+//booleans
+let x = 5;
+let y = 5;
+let z = 6;
+(x == y)       // Returns true
+(x == z)       // Returns false
