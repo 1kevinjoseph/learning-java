501020f6930bff0514894523b9f5fa9865285806
1kevinjoseph <kevinokech3@gmail.com>
2023-09-30 14:44:43 +0300

    message

--- basics/objects.js
+++ basics/objects.js
@@ -1,13 +1,26 @@
-const car = { type: "Fiat", model: "500", color: "white" };
+const car = { type: "Fiat", model: "500", color: "white" }; //object definition/contruction
+//method 1 
+const person = {
+  name: "John",
+  age: 30,
+  gender: "male",
 
-const person = { name: "John", age: 30, gender: "male" };
+};
 
-const victor = new Object();
-const victor2 = {};
-
+//method 2 of defining an object
+const victor = new Object(); 
 victor.name = "Victor";
 victor.age = 20;
 victor.gender = "male";
 victor.weight = 90;
 
 console.log(victor);
+
+const james = new Object();
+james.name = "james";
+james.age = 24;
+james.gender = "male";
+james.weight = 89;
+
+console.log(james);
+console.log(person.description())
